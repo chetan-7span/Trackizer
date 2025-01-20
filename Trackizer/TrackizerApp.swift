ceb1658369c3d3c891f71c0627306c005120c1bb
Chetan Hedamba <chetan@7span.com>
2025-01-20 18:03:08 +0530

    Initial Commit

---
+++ Trackizer/TrackizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrackizerApp.swift
+//  Trackizer
+//
+//  Created by Chetan Hedamba on 20/01/25.
+//
+
+import SwiftUI
+
+@main
+struct TrackizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
