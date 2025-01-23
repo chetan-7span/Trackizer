fb83df5f782ad7ba5b8cc605a6d00802bb747d63
Chetan Hedamba <chetan@7span.com>
2025-01-23 14:28:39 +0530

    Feat : app icon added Feat : custom circle progress bar added Feat : home screen ui completed

---
+++ Trackizer/Source/Utility/CommonViews/CircularProgressView.swift
@@ -0,0 +1,18 @@
+//
+//  CircularProgressView.swift
+//  Trackizer
+//
+//  Created by Chetan Hedamba on 23/01/25.
+//
+
+import SwiftUI
+
+struct CircularProgressView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircularProgressView()
+}
